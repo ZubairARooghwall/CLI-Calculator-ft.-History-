3c386f8dd71ff642c7d4577fe324ebe558928ba7
ZubairARooghwall <ZubairStudyTech@gmail.com>
2022-09-16 13:05:56 +0430

    history feature is almost finished

--- calculator.java
+++ calculator.java
@@ -1,90 +1,84 @@
 import java.util.Scanner;
 import java.util.Vector;
 
-public class calculator{
-    public static void main(String[] args){
-        
+public class calculator {
+    public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        
+
         double num1 = 0;
         char op;
-        double num2  = 0;
-        double result  = 0;
+        double num2 = 0;
+        double result = 0;
         int check = 0;
         int historycounter = 0;
         Vector history = new Vector();
 
-
-        while(true){
-            if(check==0){
+        while (true) {
+            if (check == 0) {
                 System.out.println("Enter the first number: ");
                 num1 = input.nextDouble();
             }
-            while(true){
-                if(check==0){ 
-            System.out.println("Enter the operator: ");
-        }
+            while (true) {
+                if (check == 0) {
+                    System.out.println("Enter the operator: ");
+                }
 
-            else{
-                System.out.println("Enter the operator:  (C for clear, E for exit, H for history)");
-        }
+                else {
+                    System.out.println("Enter the operator:  (C for clear, E for exit, H for history)");
+                }
 
-            op = input.next().charAt(0);
-            
-            if(op == '+' || op == '-' || op == '*' || op == '/' || op == 'e' || op == 'E' || op == 'H' || op == 'h' || op == 'c' || op == 'C'){
-                break;
+                op = input.next().charAt(0);
+
+                if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'e' || op == 'E' || op == 'H' || op == 'h'
+                        || op == 'c' || op == 'C') {
+                    break;
+                } else {
+                    System.out.println("Enter the right input: ");
+                }
             }
-            else {
-                System.out.println("Enter the right input: ");
+
+            if (check > 0 && op == 'h' || op == 'H') {
+                for (int a = 0, i = 0; a < check; a++, i += 4) {
+                    System.out.println(history.get(i) + " " + history.get(i + 1) + " " + history.get(i + 2) + " = "
+                            + history.get(i + 3));
+                }
+
             }
-        }
-
-        if(check > 0 && op == 'h' || op == 'H'){
-
-
-
-
-        }
 
             System.out.println("Enter the second number: ");
             num2 = input.nextDouble();
-        
-            switch(op){
-                case '+' : 
-                result = num1 + num2; 
-                break;
 
-                case '-' : 
-                result = num1 - num2; 
-                break;
+            switch (op) {
+                case '+':
+                    result = num1 + num2;
+                    break;
 
-                case '*' : 
-                result = num1 * num2; 
-                break;
+                case '-':
+                    result = num1 - num2;
+                    break;
 
-                case '/' : 
-                result = num1 / num2; 
-                break;
-                
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+
             }
 
             System.out.println("Result: " + result);
 
-
-
-
-
-
-        history.addElement(num1);
-        history.addElement(op);
-        history.addElement(num2);
-        history.addElement(result);
-
+            history.addElement(num1);
+            history.addElement(op);
+            history.addElement(num2);
+            history.addElement(result);
 
             check++;
             historycounter++;
             num1 = result;
 
-}//while (true)
-}//public main
-}//public class
+        } // while (true)
+    }// public main
+}// public class
