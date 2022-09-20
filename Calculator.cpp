e2ab94686cfacba1df8894bea0ffce2c8e301d8c
ZubairARooghwall <ZubairStudyTech@gmail.com>
2022-09-20 16:28:27 +0430

    The history feature has a bug which show the ascii value of the operators. like instead of +, it shows 43

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
     double num1, num2, result;
@@ -15,9 +18,21 @@ int main()
             std::cin >> num1;
         }
 
-        std::cout << "Enter the operator: (C for clear, E for exit, H for history, P\n";
-        std::cin >> op;
+        while (true)
+        {
+            std::cout << "Enter the operator: (C for clear, E for exit, H for history, P\n";
+            std::cin >> op;
 
+            if (op == '+' || op == '-' || op == '*' || op == '/')
+            {
+                break;
+            }
+            else if (counter > 0 && op == 'h' || op == 'H')
+            {
+                for (int a = 0, i = 0; a < counter; a++, i += 4)
+                    std::cout << history.at(i) << " " << double(history.at(i + 1)) << " " << history.at(i + 2) << " = " << history.at(i + 3) << "\n";
+            }
+        }
         std::cout << "Enter the second number: \n\n";
         std::cin >> num2;
 
@@ -38,6 +53,11 @@ int main()
         }
         std::cout << "Result: \n";
         std::cout << num1 << " " << op << " " << num2 << " = " << result << "\n\n";
+
+        history.push_back(num1);
+        history.push_back(op);
+        history.push_back(num2);
+        history.push_back(result);
 
         counter++;
         num1 = result;
